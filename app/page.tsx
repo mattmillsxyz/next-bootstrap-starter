d7385dcf770f05dbcb7a503354f08e975228fc4a
Matt Mills <mattmillsxyz@gmail.com>
2022-12-17 10:14:50 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12"></div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
